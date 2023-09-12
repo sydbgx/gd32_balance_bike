1d869fcf3746d38106640a2ad9497896233ebd8d
shiyd <sydbgx@gmail.com>
2023-09-12 16:43:54 +0800

    fetch: add user

---
+++ User/main.c
@@ -0,0 +1,39 @@
+#include "systick.h"
+#include <stdio.h>
+
+#include "bsp_led.h"
+#include "usart.h"
+#include "bsp_timer.h"
+#include "bsp_ultrasonic.h"
+#include "bsp_battery.h"
+#include "bsp_motor.h"
+#include "bsp_encoder.h"
+
+
+int main(void)
+{
+        systick_config();
+        usart_init(115200);
+
+        //printf("led test\n");
+	//bsp_led_test();
+
+	//printf("timer test\n");
+	//bsp_timer_test();
+
+	/*printf("ultrasonic test\n");
+	bsp_ultrasonic_test();*/
+
+	/*printf("battery test\n");
+	bsp_battery_test();*/
+
+	/*printf("motor test\n");
+	bsp_motor_test();*/
+
+	printf("encoder test\n");
+	bsp_encoder_test();
+
+        while (1) {
+		printf("未执行的循环\n");
+        }
+}
